fbd231b9f213c3f49d387389a7cca88110c640a2
aslatura <99444209+aslatura@users.noreply.github.com>
2022-02-15 12:07:09 +0000

    Remove geolocation button, show geolaction weather on load, change border color, change search button color

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-//to time this using the API time stamp for the city selected,
-//see Week 7 Lesson 6 minute 7
+/////////////////////////////////////////////////////////
 
-//format of description in css:
-//#description::first-letter {
-//  text-transform: uppercase;}
+//Current day and time on load
+
+/////////////////////////////////////////////////////////
 
 function formatDate(date) {
   let hours = date.getHours();
@@ -30,37 +29,29 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-
-  getWeather(cityInput.value);
-}
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+/////////////////////////////////////////////////////////
 
-////////////////
+//Load current weather in person's location on load
 
-function getWeather(city) {
+/////////////////////////////////////////////////////////
+
+function searchLocation(position) {
   let apiKey = "d3592968d288237ab5de304e493c66f3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
-  axios.get(`${apiUrl}&q=${city}&appid=${apiKey}`).then(displayOnPage);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayCurrentOnPage);
 }
 
-navigator.geolocation.getCurrentPosition(getWeather);
+function getCurrentLocation() {
+  //event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 
-function displayOnPage(response) {
-  //console.log(response.data.main.temp);
-  //console.log(response.data.main.humidity);
-  //console.log(response.data.wind.speed);
-  //console.log(response.data.weather[0].main);
+function displayCurrentOnPage(response) {
   document.querySelector("#city").innerHTML = response.data.name;
 
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -79,7 +70,53 @@ function displayOnPage(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = response.data.main.temp;
-  //farenheitTemperature = response.data.main;
+}
+
+/////////////////////////////////////////////////////////
+
+//Weather for city being searched
+
+/////////////////////////////////////////////////////////
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+
+  getWeather(cityInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function getWeather(city) {
+  let apiKey = "d3592968d288237ab5de304e493c66f3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
+  axios.get(`${apiUrl}&q=${city}&appid=${apiKey}`).then(displayOnPage);
+}
+
+navigator.geolocation.getCurrentPosition(getWeather);
+
+function displayOnPage(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  //document.querySelector("#icon").innerHTML = response.data.main
+  let iconElement = document.querySelector("#weatherIcon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  celsiusTemperature = response.data.main.temp;
 
   function search(event) {
     event.preventDefault();
@@ -91,28 +128,6 @@ function displayOnPage(response) {
 }
 
 //////////////////////
-
-function searchLocation(position) {
-  let apiKey = "d3592968d288237ab5de304e493c66f3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayOnPage);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-function searchGeo(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-}
 
 //Celsius and farenheit temperatuere displays
 
@@ -141,5 +156,3 @@ function showCelsius(event) {
   celsiusLink.classList.add("active");
   farenheitLink.classList.remove("active");
 }
-
-//Need something here that searches a city on load
